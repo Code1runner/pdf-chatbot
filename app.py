78fe683b35a388bae96ca179c31c4624581f70a0
Code1runner <michalgabb98@gmail.com>
2023-10-19 21:11:08 +0200

    add pdf reader

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from pypdf import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -9,6 +10,16 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size = 1000,
+        chunk_overlap = 200,
+        length_function = len 
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 def main():
     load_dotenv()
@@ -24,7 +35,8 @@ def main():
         if st.button("Upload"):
             with st.spinner("Uploading"):
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
-
+                
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
 if __name__ == '__main__':
     main()
